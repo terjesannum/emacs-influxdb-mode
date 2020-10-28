e7a0ebfb262eaefac5a252a01c475e0bc85dd3e9
Terje Sannum <terje.sannum@nav.no>
2020-10-28 07:01:55 +0100

    set comint-input-ring-size

--- influxdb-mode.el
+++ influxdb-mode.el
@@ -36,6 +36,7 @@
 (defvar influxdb-directory nil "Directory where influx cli is started. Set this if you want to always start the influx cli from a specific directory.")
 (defvar influxdb-mode-hook nil)
 (defvar influxdb-history-file-name "~/.influx_history")
+(defvar influxdb-input-ring-size 100000 "Size of input history ring in influxdb buffer.")
 
 (defvar influxdb-mode-hook nil)
 
@@ -49,6 +50,7 @@
     (unless (comint-check-proc "*influx*")
       (make-comint "influx" influxdb-cli nil "-host" influxdb-host "-port" (format "%s" influxdb-port) "-database" influxdb-database "-precision" influxdb-precision))
     (switch-to-buffer "*influx*")
+    (setq comint-input-ring-size influxdb-input-ring-size)
     (setq comint-input-ring (make-ring comint-input-ring-size))
     (setq comint-input-ring-file-name influxdb-history-file-name)
     (comint-read-input-ring 'silent)
